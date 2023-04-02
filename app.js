77a8902152575b7d6179c3f7c1eda58e46ff75b5
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-04-02 15:50:28 -0400

    second commit

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
+//create an object called players
+  //the key will be the players name
+  //the value will be an array of the  players previous scores
+    //scores will be added using the push method
+    //reference back to last score using .length -1
+    //run a highscore function that compares current score to previous scores. If current score is highest score, return "NEW HIGH SCORE"
+
+
+
+
 function guessANumber(){
-var name = prompt("What's Your Name?")
+  let secretNumber = Math.floor(Math.random() * 10) + 1;
+  let name = prompt("What's Your Name?");
 
-  var input = prompt(`${name}, Guess a Number`);
-  var guess = Number(input);
-  var secretNumber = Math.floor(Math.random() * 10) + 1;
-  var tries = 1;
-  var history = [guess];
+  if (name == null){
+    alert ('Goodbye')
+    return;
+  }
 
-  while (!Number.isInteger(guess)){
-  guess = Number(prompt ("Please input valid number"));
-  };
+  let input = prompt(`${name}, Guess a Number`);
+  let guess = input;
+  let tries = 1;
+  let history = [guess];
 
-  while (guess !== secretNumber) {
-    if (guess === 0) {
-      alert ("Later Loser");
+
+
+  while (guess != secretNumber) {
+    if (guess === null ){
+      alert ("Goodbye");
       return;
-    }
-    if (guess > secretNumber){
-      guess = Number(prompt("Too High!"))
+    } else if (!Number.isInteger(Number(guess))){
+      guess = Number(prompt ("Please input valid number"));
+    } else if (guess > secretNumber){
+      guess = prompt(`Sorry ${name}, Too High!`);
     } else if (guess < secretNumber){
-      guess = Number(prompt("Too Low"))
-    }
+      guess = prompt(`Sorry ${name}, Too Low!`);
+    } else {
+      prompt (`${name}, please enter a number between 1 and 10.`)
+    };
   history.push(guess);
   tries++;
   };
 
 console.log(tries);
 console.log(history);
+
   alert (`Congrats ${name}! It only took you ${tries} tries and your previous guesses were ${history}`);
   let playAgain = prompt("Play again? (y/n)").toLowerCase();
    if (playAgain === "n"){
-    alert ("Later Loser!");
+    alert ("Goodbye");
+    return;
    } else {
-    let input = prompt(`${name}, Guess a Number`)
+    guessANumber()
    }
 }
