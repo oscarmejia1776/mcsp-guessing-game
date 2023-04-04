5887b33d9b2a71c8495be8e8821f9aa909562a9c
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-04-04 12:48:43 -0400

    #7 Specification Working Code

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 //create an object called players
-  //the key will be the players name
-  //the value will be an array of the  players previous scores
-    //scores will be added using the push method
-    //reference back to last score using .length -1
-    //run a highscore function that compares current score to previous scores. If current score is highest score, return "NEW HIGH SCORE"
+//the key will be the players name
+//the value will be an array of the  players previous scores
+//scores will be added using the push method
+//reference back to last score using .length -1
+//run a highscore function that compares current score to previous scores. If current score is highest score, return "NEW HIGH SCORE"
 
+var players = {};
 
-
-
-function guessANumber(){
+function guessANumber() {
   let secretNumber = Math.floor(Math.random() * 10) + 1;
   let name = prompt("What's Your Name?");
 
-  if (name == null){
-    alert ('Goodbye')
+  if (name == null) {
+    alert("Goodbye");
     return;
   }
 
@@ -22,34 +21,54 @@ function guessANumber(){
   let tries = 1;
   let history = [guess];
 
-
-
   while (guess != secretNumber) {
-    if (guess === null ){
-      alert ("Goodbye");
+    if (guess === null) {
+      alert("Goodbye");
       return;
-    } else if (!Number.isInteger(Number(guess))){
-      guess = Number(prompt ("Please input valid number"));
-    } else if (guess > secretNumber){
+    } else if (!Number.isInteger(Number(guess))) {
+      guess = Number(prompt("Please input valid number"));
+    } else if (guess > secretNumber) {
       guess = prompt(`Sorry ${name}, Too High!`);
-    } else if (guess < secretNumber){
+    } else if (guess < secretNumber) {
       guess = prompt(`Sorry ${name}, Too Low!`);
     } else {
-      prompt (`${name}, please enter a number between 1 and 10.`)
-    };
-  history.push(guess);
-  tries++;
-  };
+      prompt(`${name}, please enter a number between 1 and 10.`);
+    }
+    history.push(guess);
+    tries++;
+  }
 
-console.log(tries);
-console.log(history);
+  console.log(tries);
+  console.log(history);
 
-  alert (`Congrats ${name}! It only took you ${tries} tries and your previous guesses were ${history}`);
+  if (players[name] !== undefined) {
+    if (history.length > players[name].length) {
+      alert(
+        `That’s Correct ${name}! You did better in your last game by ${
+          history.length - players[name].length
+        } fewer guesses!`
+      );
+    } else if (players[name].length > history.length) {
+      alert(
+        `That’s Correct ${name}! And you beat your previous attempt by ${
+          players[name].length > history.length
+        } fewer guesses.`
+      );
+    }
+  } else {
+    alert(
+      `Congrats ${name}! It only took you ${tries} tries and your previous guesses were ${history}`
+    );
+  }
+
+  players[name] = history;
+  console.log(players);
+
   let playAgain = prompt("Play again? (y/n)").toLowerCase();
-   if (playAgain === "n"){
-    alert ("Goodbye");
+  if (playAgain === "n") {
+    alert("Goodbye");
     return;
-   } else {
-    guessANumber()
-   }
+  } else {
+    guessANumber();
+  }
 }
